03866567b5fabc00ac4250a3265817da62b49df6
Arthur Valerio De Santi <arthurvaleriodesanti@gmail.com>
2026-05-22 00:43:27 -0300

    criando orderId e validações

--- backend/functions/src/orders/handlers/cancelOffer.ts
+++ backend/functions/src/orders/handlers/cancelOffer.ts
@@ -6,3 +6,19 @@ import {onCall, HttpsError} from "firebase-functions/v2/https";
 import * as admin from "firebase-admin";
 import {requireAuthenticatedUser} from "../../shared/auth";
 import {db} from "../../shared/firebase";
+
+// cancelar oferta
+export const cancelOffer =onCall(async (request) =>{
+    requireAuthenticatedUser(request);
+
+    // fica mais facil pegar o orderId que não existe dentro da coleção orders
+    // então precisa criar
+    const orderId = request.data.orderId;
+    const uid = request.auth!.uid;
+
+    if(!orderId) throw new HttpsError("invalid-argument", "camp obrigatorio faltando");
+
+    const ordToCanc = await db.collection("orders").doc(orderId).get();
+    if(!ordToCanc) throw new HttpsError("not-found", "Ordem não encontrada");
+
+});
