1e0135ef9687ae90bebacd72777b57b6a633b3ec
Arthur Valerio de Santi <arthur.santi@ima.sp.gov.br>
2026-05-22 15:29:45 -0300

    function cancelOffer e deploy

--- backend/functions/src/orders/handlers/cancelOffer.ts
+++ backend/functions/src/orders/handlers/cancelOffer.ts
@@ -3,7 +3,6 @@
 // Descrição: Handlers para as ofertas de compra e venda e sistema de matching.
 
 import {onCall, HttpsError} from "firebase-functions/v2/https";
-import * as admin from "firebase-admin";
 import {requireAuthenticatedUser} from "../../shared/auth";
 import {db} from "../../shared/firebase";
 
@@ -13,12 +12,32 @@ export const cancelOffer =onCall(async (request) =>{
 
     // fica mais facil pegar o orderId que não existe dentro da coleção orders
     // então precisa criar
-    const orderId = request.data.orderId;
+    const {orderId} = request.data;
     const uid = request.auth!.uid;
 
-    if(!orderId) throw new HttpsError("invalid-argument", "camp obrigatorio faltando");
+    // verifica se existe o orderId fornecido, em erro joga invalid argument
+    if(!orderId.exists) throw new HttpsError("invalid-argument", "camp obrigatorio faltando");
 
-    const ordToCanc = await db.collection("orders").doc(orderId).get();
-    if(!ordToCanc) throw new HttpsError("not-found", "Ordem não encontrada");
+    try{
+        // declara ordToCanc (ordem para cancelar) e espera o firestore para pegar as informações
+        const ordToCanc = await db.collection("orders").doc(orderId).get();
+        if(!ordToCanc) throw new HttpsError("not-found", "Ordem não encontrada"); // se não vier nada, não achou
 
+        // pega os dados da ordem
+        const orderData = ordToCanc.data()!; // garante que vai vir um não null (tratamento ali em cima)
+        // se a ordem e o usuario que quis cancelar não tiverem o mesmo UID
+        // a permissão vai ser negada
+        if(orderData.userId != uid) throw new HttpsError("permission-denied", "Usuario invalido");
+
+        // verifica se o status não é open, se sim ele joga erro, no caso de ser open ele procede
+        if(!orderData.status.compareTo("open")) throw new HttpsError("failed-precondition", "Ordem não esta aberta");
+
+        // atualiza o documento para cancelado
+        await db.collection("orders").doc(orderId).update({status: "cancelled"});
+
+        return {sucess: true};
+    }catch(e){
+        if(e instanceof HttpsError) throw e;
+        throw new HttpsError("internal", `Erro: ${e}`);
+    }
 });
